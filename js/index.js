ca33119a4d9dda50ab83a7d0c03098438ef23850
s3211t <s3211t@gmail.com>
2019-08-14 18:29:34 +0800

    遊戲初版

---
+++ js/index.js
@@ -0,0 +1,17 @@
+const config = {
+    type: Phaser.AUTO,
+    width: w,
+    height: h,
+    parent: 'app',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {//重力值
+                y: 700
+            },
+            debug:true,
+        },
+    },
+    scene: [gameStart,gamePlay]
+}
+const game = new Phaser.Game(config);
